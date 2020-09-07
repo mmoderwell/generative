d6da99c8d5dcc1bc23f4310fb3a874cdbb6aaf28
Matt Moderwell <mmoderwell@gmail.com>
2020-09-07 09:39:37 -0500

    Initial commit

---
+++ visual/grayscale.py
@@ -0,0 +1,15 @@
+import cv2
+
+# Read Image
+img = cv2.imread('input.jpg')
+
+# Display Image
+# cv2.imshow('image',img)
+#cv2.waitKey(0)
+#cv2.destroyAllWindows()
+
+# Applying Grayscale filter to image
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# Saving filtered image to new file
+cv2.imwrite('grayscale.jpg',gray)
